5ce2243fb4893806bf0936ee45c4e08f1972ec85
coderneosmith <coder.neo.smith@gmail.com>
2013-11-24 13:23:01 +0000

    added bar animation, refactored
    Signed-off-by: coderneosmith <coder.neo.smith@gmail.com>

--- README.md
+++ README.md
@@ -33,6 +33,37 @@ OSX:
  * safari 6.1    
  * Firefox 25.0    
  * Chrome 31.0.1650.57    
- * Opera 12.16    
+ * Opera 12.16 
+
+### Notes
+
+To create big binary file with random staff in it (10M) except first bytes which should be "foo.bar" \x66\x6f\x6f\x0a\x62\x61\x72 
+
+```
+// create file
+head -c 10M </dev/urandom > test.bin
+
+// Then override the first 7 bytes with 
+
+printf '\x66\x6f\x6f\x0a\x62\x61\x72' | dd conv=notrunc of=test.bin bs=7 seek=$((0x00000000))
+```
+
+To create big text file with filled with "1" (10M) except first chars which should be "foo.bar"
+
+
+```
+// create file
+tr -dc A-Za-z0-9 </dev/urandom |  head -c 10M > test.txt
+
+// Then change first 7 chars to foo.bar 
+
+!!!
+cat test.txt | sed 's/^......./foo.bar/' > test.txt
+```
+
+So for the test first 7 elements in data:
+* for binary should be 102,111,111,46,98,97,114
+* for test should be foo.bar   
+
 
 
--- js/progress.js
+++ js/progress.js
@@ -10,38 +10,65 @@ function Progress() {
     "use strict";
 
     this.size = null;
+    this.t0 = null;
 }
 
 Progress.prototype = (function() {
 
     "use strict";
 
-    function _get(url, onprogress, onerror, onload) {
+    function _get( url, opt ) {
+
         var self = this,
+            onstart = opt.onstart || function(){},
+            onprogress = opt.onprogress || function(){},
+            onerror = opt.onerror || function(){},
+            onload = opt.onload || function(){},
+            binary = opt.binary || false,
             request = new XMLHttpRequest();
+
+        self.t0 = new Date().getTime();
+
+        onstart();
 
         request.addEventListener("load", function() {
             if (!self.size) {
                 self.size = Number(request.getResponseHeader("Content-Length"));
             }
-            var data = new Uint8Array(request.response);
-
+            
             // hack for opera opera sometimes does not fire progress with 100% - so we fire it here to make sure that last progress event will report 100%
             if (window.navigator.userAgent.indexOf("Opera") !== -1) {
                 onprogress({
                     lengthComputable: true,
                     loaded: self.size,
-                    total: self.size
+                    total: self.size,
+                    t0: self.t0,
+                    t1: new Date().getTime(),
+                    t2:0,
+                    percent: 100
                 });
             }
             // end of a hack
-            
-            onload(data);
+            if(binary){
+                onload(new Uint8Array(request.response));
+            }else{
+                onload(request.responseText);
+            }
         }, false);
-        request.addEventListener("progress", onprogress, false);
+        request.addEventListener("progress", function(e){
+            e.t0 = self.t0;
+            e.t1 = new Date().getTime();
+            e.percent = (e.loaded / e.total * 100);
+            e.t2 =  e.t0 - e.t1 + ( e.total*(e.t1 - e.t0)) /e.loaded;
+            onprogress(e); 
+        }, false);
         request.addEventListener("error", onerror, false);
         request.open("GET", url);
-        request.responseType = "arraybuffer";
+        if(binary){
+            request.responseType = "arraybuffer";
+        }else{
+            request.overrideMimeType('text/plain; charset=x-user-defined');
+        }
         request.send();
     }
 
